60d5e5a6a7e83a574267bc92c9de7cf5cf894623
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-10-03 04:31:53 +0900

    add: sln、mainプログラム追加

---
+++ Src/Pch.h
@@ -0,0 +1,102 @@
+﻿#pragma once
+
+/**************************************************
+* 
+* Precompile Header File and forced include file
+* 
+**************************************************/
+
+
+
+/**************************************************
+* Windows
+**************************************************/
+
+// Socket (Must be included before windows.h)
+// disable MIN MAX macro
+#define NOMINMAX
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+// Windows
+#include <windows.h>
+// ComPtr
+#include <wrl/client.h>
+
+
+
+/**************************************************
+* DirectX
+**************************************************/
+
+#include <d3d11_4.h>
+#include <dxgi1_6.h>
+#pragma comment(lib,"d3d11.lib")
+#pragma comment(lib,"dxgi.lib")
+//#pragma comment(lib,"dxguid.lib")
+
+// DirectX Tex
+#include <DirectXTex.h>
+#pragma comment(lib, "DirectXTex.lib")
+
+// DirectX Tool Kit
+#include <Audio.h>
+#include <SimpleMath.h>
+#pragma comment(lib, "DirectXTKAudioWin8.lib")
+#pragma comment(lib, "DirectXTK.lib")
+
+
+
+/**************************************************
+* strconv
+**************************************************/
+
+#include "strconv.h"
+
+
+
+/**************************************************
+* Library
+**************************************************/
+
+#include "Utility/Assert.h"
+#include "Utility/Macro.h"
+#include "Utility/Memory.h"
+#include "Utility/StdC++.h"
+#include "Utility/Templates.h"
+
+#include "System/DirectX11/DirectX11System.h"
+#include "System/DirectX11/DirectX11BufferSystem.h"
+#include "System/DirectX11/DirectX11TextureSystem.h"
+
+
+
+/**************************************************
+* Framework
+**************************************************/
+
+namespace Math = DirectX::SimpleMath;
+
+// マテリアル
+#include "Framework/Direct3D/KdMaterial.h"
+// メッシュ
+#include "Framework/Direct3D/KdMesh.h"
+// モデル
+#include "Framework/Direct3D/KdModel.h"
+// 基底ポリゴン
+#include "Framework/Direct3D/KdPolygon.h"
+// 板ポリゴン
+#include "Framework/Direct3D/KdSquarePolygon.h"
+// アニメーション
+#include "Framework/Math/KdAnimation.h"
+
+// シェーダー
+#include "System/Shader/ShaderManager.h"
+
+/**************************************************
+* Application
+**************************************************/
+
+#include "Application/Application.h"
+
+
---
+++ Src/main.cpp
@@ -0,0 +1,17 @@
+﻿#include "Application/Application.h"
+
+int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
+{
+    hInstance; hPrevInstance; lpCmdLine; nShowCmd; // C4100 'identifier' : unreferenced formal parameter
+    
+    // インスタンスを取得
+    auto& app = Application::Instance();
+
+    // 初期化
+    if (!app.Init()) return -1;
+
+    // 実行
+    app.Run();
+
+    return 0;
+}
