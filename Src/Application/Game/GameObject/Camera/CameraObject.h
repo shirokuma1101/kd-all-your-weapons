63d722ed65016753ea37f41f64542ed126744019
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-10-04 07:21:56 +0900

    update: カメラがメッシュに埋まらないように修正

--- Src/Application/Game/GameObject/Camera/CameraObject.h
+++ Src/Application/Game/GameObject/Camera/CameraObject.h
@@ -13,30 +13,57 @@ public:
     }
 
     virtual void Update(float delta_time) override {
-        auto& target = m_wpFollowingTarget.lock()->GetTransform();
-        m_transform.position = target.position;
-        m_transform.rotation = target.rotation;
-        m_spCamera->SetMatrix(m_localMatrix * m_transform.Composition());
-        m_spCamera->SetToShader();
-    }
+        /* メッシュに埋まらないように光線を利用して当たり判定を行う */
+        static const float ray_offset = 0.1f; // ターゲットとカメラの距離に対するオフセット
+        
+        // ターゲットの位置と回転のみ保存
+        auto& target_transform = m_wpFollowingTarget.lock()->GetTransform();
+        m_transform.position = target_transform.position;
+        m_transform.rotation = target_transform.rotation;
 
-    virtual const DirectX::SimpleMath::Matrix GetMatrix() const noexcept {
-        return m_localMatrix * m_transform.matrix;
+        // ローカル行列を掛けた行列を作成
+        Math::Matrix compositied = m_localTransform.matrix * m_transform.Composition();
+
+        // プレイヤーからカメラの位置への方向ベクトルを取得
+        Math::Vector3 dir = compositied.Translation() - m_transform.matrix.Translation();
+        dir.Normalize();
+
+        // 当たり判定
+        std::list<collision::Result> results;
+        collision::Ray ray(m_transform.matrix.Translation(), dir, m_localTransform.matrix.Translation().Length() + ray_offset);
+        for (const auto& e : Application::Instance().GetGameSystem()->GetGameObjects()) {
+            const auto& collider = e->GetCollider();
+            if (!collider) continue;
+            // 判定 (光線)
+            if (collider->Intersects(DefaultCollisionType::Bump, e->GetTransform().matrix, ray, &results)) {
+                if (auto result = collision::GetFarthest(results); result) {
+                    m_transform.position += result->direction * result->depth;
+                }
+                results.clear();
+            }
+        }
+
+        // 当たり判定後に再計算
+        m_spCamera->SetMatrix(m_localTransform.matrix * m_transform.Composition());
+        m_spCamera->SetToShader();
     }
 
     virtual std::shared_ptr<CameraSystem> GetCamera() final {
         return m_spCamera;
     }
 
-    virtual void SetFollowingTarget(std::shared_ptr<GameObject> obj, const Math::Matrix& local_mat = {}) final {
+    virtual void SetFollowingTarget(std::shared_ptr<GameObject> obj, const Transform& local_transform = {}) final {
         m_wpFollowingTarget = obj;
-        m_localMatrix = local_mat;
+        // ローカルの位置と回転のみ保存
+        m_localTransform.position = local_transform.position;
+        m_localTransform.rotation = local_transform.rotation;
+        m_localTransform.Composition();
     }
 
 protected:
 
     std::shared_ptr<CameraSystem> m_spCamera;
     std::weak_ptr<GameObject>     m_wpFollowingTarget;
-    Math::Matrix                  m_localMatrix;
+    Transform                     m_localTransform;
 
 };
