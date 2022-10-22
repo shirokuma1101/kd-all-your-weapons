e9fdf3784b865283d44c155e528ceed7f1b88a82
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-10-22 23:20:41 +0900

    update: 命名を統一、不要な関数を削除、SpriteFontクラスを追加

--- Src/System/Shader/SpriteShader/SpriteShader.cpp
+++ Src/System/Shader/SpriteShader/SpriteShader.cpp
@@ -2,16 +2,10 @@
 
 bool SpriteShader::Init()
 {
-    //-------------------------------------
-    // 頂点シェーダ
-    //-------------------------------------
     {
-        // コンパイル済みのシェーダーヘッダーファイルをインクルード
-        #include "SpriteShader_VS.inc"
-
-        // 頂点シェーダー作成
-        if (FAILED(DirectX11System::Instance().GetDev()->CreateVertexShader(compiledBuffer, sizeof(compiledBuffer), nullptr, &m_VS))) {
-            assert(0 && "頂点シェーダー作成失敗");
+        #include "SpriteShaderVS.inc"
+        if (FAILED(DirectX11System::Instance().GetDev()->CreateVertexShader(compiledBuffer, sizeof(compiledBuffer), nullptr, &m_pVS))) {
+            assert::RaiseAssert(ASSERT_FILE_LINE, "頂点シェーダー作成失敗");
             Release();
             return false;
         }
@@ -25,143 +19,74 @@ bool SpriteShader::Init()
         // 頂点インプットレイアウト作成
         if (FAILED(DirectX11System::Instance().GetDev()->CreateInputLayout(
             &layout[0],
-            layout.size(),
+            static_cast<UINT>(layout.size()),
             compiledBuffer,
             sizeof(compiledBuffer),
-            &m_VLayout))
-        ){
-            assert(0 && "CreateInputLayout失敗");
+            &m_pInputLayout
+        ))) {
+            assert::RaiseAssert(ASSERT_FILE_LINE, "頂点インプットレイアウト作成失敗");
             Release();
             return false;
         }
     }
-
-    //-------------------------------------
-    // ピクセルシェーダ
-    //-------------------------------------
     {
-        // コンパイル済みのシェーダーヘッダーファイルをインクルード
-        #include "SpriteShader_PS.inc"
-
-        if (FAILED(DirectX11System::Instance().GetDev()->CreatePixelShader(compiledBuffer, sizeof(compiledBuffer), nullptr, &m_PS))) {
-            assert(0 && "ピクセルシェーダー作成失敗");
+        #include "SpriteShaderPS.inc"
+        if (FAILED(DirectX11System::Instance().GetDev()->CreatePixelShader(compiledBuffer, sizeof(compiledBuffer), nullptr, &m_pPS))) {
+            assert::RaiseAssert(ASSERT_FILE_LINE, "ピクセルシェーダー作成失敗");
             Release();
             return false;
         }
     }
 
-    //-------------------------------------
-    // 定数バッファ作成
-    //-------------------------------------
-    m_cb0.Create();
-    m_cb1.Create();
-
-    //-------------------------------------
-    // ステート作成
-    //-------------------------------------
-    m_smp0_Point = directx11_helper::CreateSamplerState(
-        DirectX11System::Instance().GetDev().Get(), directx11_helper::SamplerFilterMode::Point, 0, directx11_helper::SamplerAddressMode::Wrap, false
-    );
-    m_smp0_Linear = directx11_helper::CreateSamplerState(
-        DirectX11System::Instance().GetDev().Get(), directx11_helper::SamplerFilterMode::Linear, 0, directx11_helper::SamplerAddressMode::Wrap, false
-    );
-
-    m_ds = directx11_helper::CreateDepthStencilState(DirectX11System::Instance().GetDev().Get(), false, false);
-    m_rs = directx11_helper::CreateRasterizerState(DirectX11System::Instance().GetDev().Get(), D3D11_FILL_SOLID, D3D11_CULL_NONE, true, false);
-
+    m_spriteCB.Create();
+    m_projectionCB.Create();
+    
     return true;
 }
 
-void SpriteShader::Begin(bool linear, bool disable_zbuffer)
+void SpriteShader::Release()
 {
-    //---------------------------------------
-    // 2D用正射影行列作成
-    //---------------------------------------
-    UINT pNumVierports = 1;
-    D3D11_VIEWPORT vp;
-    DirectX11System::Instance().GetCtx()->RSGetViewports(&pNumVierports, &vp);
-    m_mProj2D = DirectX::XMMatrixOrthographicLH(vp.Width, vp.Height, 0, 1);
-
-    // 定数バッファ書き込み
-    m_cb1.Get()->mProj = m_mProj2D;
-    m_cb1.Write();
-
-    //---------------------------------------
-    // 現在のステート記憶
-    //---------------------------------------
-    // DepthStencilState記憶
-    DirectX11System::Instance().GetCtx()->OMGetDepthStencilState(&m_saveState.DS, &m_saveState.StencilRef);
-    // RasterizerState記憶
-    DirectX11System::Instance().GetCtx()->RSGetState(&m_saveState.RS);
-    // SamplerState記億
-    DirectX11System::Instance().GetCtx()->PSGetSamplers(0, 1, &m_saveState.Smp0);
-
-    //---------------------------------------
-    // 使用するステートをセット
-    //---------------------------------------
-    // Z判定、Z書き込み無効のステートをセット
-    if (disable_zbuffer) {
-        DirectX11System::Instance().GetCtx()->OMSetDepthStencilState(m_ds, 0);
-    }
-    // Samplerステートをセット
-    if (linear) {
-        DirectX11System::Instance().GetCtx()->PSSetSamplers(0, 1, &m_smp0_Linear);
-    }
-    else {
-        DirectX11System::Instance().GetCtx()->PSSetSamplers(0, 1, &m_smp0_Point);
-    }
-    // Rasterizerステートをセット
-    DirectX11System::Instance().GetCtx()->RSSetState(m_rs);
-
-    //---------------------------------------
-    // シェーダ
-    //---------------------------------------
-
-    // シェーダをセット
-    DirectX11System::Instance().GetCtx()->VSSetShader(m_VS, 0, 0);
-    DirectX11System::Instance().GetCtx()->PSSetShader(m_PS, 0, 0);
-
-    // 頂点レイアウトセット
-    DirectX11System::Instance().GetCtx()->IASetInputLayout(m_VLayout);
-
-    // 定数バッファセット
-    DirectX11System::Instance().GetCtx()->VSSetConstantBuffers(2, 1, m_cb0.GetBufferAddress());
-    DirectX11System::Instance().GetCtx()->PSSetConstantBuffers(2, 1, m_cb0.GetBufferAddress());
-    DirectX11System::Instance().GetCtx()->VSSetConstantBuffers(3, 1, m_cb1.GetBufferAddress());
-    DirectX11System::Instance().GetCtx()->PSSetConstantBuffers(3, 1, m_cb1.GetBufferAddress());
+    m_spriteCB.Release();
+    m_projectionCB.Release();
+    
+    memory::SafeRelease(&m_pVS);
+    memory::SafeRelease(&m_pInputLayout);
+    memory::SafeRelease(&m_pPS);
 }
 
-void SpriteShader::End()
+void SpriteShader::SetToDevice()
 {
-    //---------------------------------------
-    // 記憶してたステートに戻す
-    //---------------------------------------
-    DirectX11System::Instance().GetCtx()->OMSetDepthStencilState(m_saveState.DS, m_saveState.StencilRef);
-    DirectX11System::Instance().GetCtx()->RSSetState(m_saveState.RS);
-    DirectX11System::Instance().GetCtx()->PSSetSamplers(0, 1, &m_saveState.Smp0);
-    memory::SafeRelease(&m_saveState.DS);
-    memory::SafeRelease(&m_saveState.RS);
-    memory::SafeRelease(&m_saveState.Smp0);
+    UINT viewport = 1;
+    D3D11_VIEWPORT vp{};
+    DirectX11System::Instance().GetCtx()->RSGetViewports(&viewport, &vp);
+    
+    m_projectionCB.Get()->projection = DirectX::XMMatrixOrthographicLH(vp.Width, vp.Height, 0, 1);
+    m_projectionCB.Write();
+
+    DirectX11System::Instance().GetCtx()->VSSetShader(m_pVS, nullptr, 0);
+    DirectX11System::Instance().GetCtx()->PSSetShader(m_pPS, nullptr, 0);
+    DirectX11System::Instance().GetCtx()->IASetInputLayout(m_pInputLayout);
+
+    DirectX11System::Instance().GetCtx()->VSSetConstantBuffers(2, 1, m_spriteCB.GetBufferAddress());
+    DirectX11System::Instance().GetCtx()->PSSetConstantBuffers(2, 1, m_spriteCB.GetBufferAddress());
+    DirectX11System::Instance().GetCtx()->VSSetConstantBuffers(3, 1, m_projectionCB.GetBufferAddress());
+    DirectX11System::Instance().GetCtx()->PSSetConstantBuffers(3, 1, m_projectionCB.GetBufferAddress());
 }
 
 void SpriteShader::DrawTex(const DirectX11Texture& tex, const Math::Vector2& pos, const Math::Vector2& rect, const Math::Rectangle* src_rect, const Math::Color& color, const Math::Vector2& pivot)
 {
-    // テクスチャ(ShaderResourceView)セット
     DirectX11System::Instance().GetCtx()->PSSetShaderResources(0, 1, tex.GetSrvAddress());
-
-    // 色
-    m_cb0.Get()->Color = color;
-    m_cb0.Write();
-
-    // UV
-    Math::Vector2 uvMin = { 0, 0 };
-    Math::Vector2 uvMax = { 1, 1 };
+    
+    m_spriteCB.Get()->color = color;
+    m_spriteCB.Write();
+    
+    Math::Vector2 uv_min = { 0, 0 };
+    Math::Vector2 uv_max = { 1, 1 };
     if (src_rect) {
-        uvMin.x = src_rect->x / (float)tex.GetTextureDesc().Width;
-        uvMin.y = src_rect->y / (float)tex.GetTextureDesc().Height;
-
-        uvMax.x = (src_rect->x + src_rect->width) / (float)tex.GetTextureDesc().Width;
-        uvMax.y = (src_rect->y + src_rect->height) / (float)tex.GetTextureDesc().Height;
+        uv_min.x = src_rect->x / (float)tex.GetTextureDesc().Width;
+        uv_min.y = src_rect->y / (float)tex.GetTextureDesc().Height;
+        uv_max.x = (src_rect->x + src_rect->width) / (float)tex.GetTextureDesc().Width;
+        uv_max.y = (src_rect->y + src_rect->height) / (float)tex.GetTextureDesc().Height;
     }
 
     // 頂点作成
@@ -170,22 +95,19 @@ void SpriteShader::DrawTex(const DirectX11Texture& tex, const Math::Vector2& pos
     float x2 = (float)(pos.x + rect.x);
     float y2 = (float)(pos.y + rect.y);
 
-    // 基準点(Pivot)ぶんずらす
+    // 基準点(Pivot)分ずらす
     x1 -= pivot.x * rect.x;
     x2 -= pivot.x * rect.x;
     y1 -= pivot.y * rect.y;
     y2 -= pivot.y * rect.y;
 
     Vertex vertex[] = {
-        { {x1, y1, 0},	{uvMin.x, uvMax.y} },
-        { {x1, y2, 0},	{uvMin.x, uvMin.y} },
-        { {x2, y1, 0},	{uvMax.x, uvMax.y} },
-        { {x2, y2, 0},	{uvMax.x, uvMin.y} }
-
+        { {x1, y1, 0},	{uv_min.x, uv_max.y} },
+        { {x1, y2, 0},	{uv_min.x, uv_min.y} },
+        { {x2, y1, 0},	{uv_max.x, uv_max.y} },
+        { {x2, y2, 0},	{uv_max.x, uv_min.y} },
     };
-
-    // 描画
-    DirectX11System::Instance().DrawVertices(D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP, 4, sizeof(Vertex), vertex);
+    DirectX11System::Instance().DrawVertices(D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP, 4, sizeof(vertex), vertex);
 
     // セットしたテクスチャを解除しておく
     ID3D11ShaderResourceView* srv = nullptr;
@@ -194,120 +116,53 @@ void SpriteShader::DrawTex(const DirectX11Texture& tex, const Math::Vector2& pos
 
 void SpriteShader::DrawPoint(const Math::Vector2& pos, const Math::Color& color)
 {
-    // 白テクスチャ
     DirectX11System::Instance().GetCtx()->PSSetShaderResources(0, 1, DirectX11System::Instance().GetWhiteTexture()->GetSrvAddress());
 
-    // 色
-    m_cb0.Get()->Color = color;
-    m_cb0.Write();
+    m_spriteCB.Get()->color = color;
+    m_spriteCB.Write();
     
-    // 描画
     Vertex vertex[] = {
-        { {pos.x, pos.y, 0}, {0, 0} },
+        { {pos.x, pos.y, 0},{0, 0} },
     };
-    DirectX11System::Instance().DrawVertices(D3D_PRIMITIVE_TOPOLOGY_POINTLIST, 1, sizeof(Vertex), vertex);
+    DirectX11System::Instance().DrawVertices(D3D_PRIMITIVE_TOPOLOGY_POINTLIST, 1, sizeof(vertex), vertex);
 }
 
 void SpriteShader::DrawLine(const Math::Vector2& pos1, const Math::Vector2& pos2, const Math::Color& color)
 {
-    // 白テクスチャ
     DirectX11System::Instance().GetCtx()->PSSetShaderResources(0, 1, DirectX11System::Instance().GetWhiteTexture()->GetSrvAddress());
 
-    // 色
-    m_cb0.Get()->Color = color;
-    m_cb0.Write();
+    m_spriteCB.Get()->color = color;
+    m_spriteCB.Write();
     
-    // 描画
     Vertex vertex[] = {
         { {pos1.x, pos1.y, 0}, {0, 0} },
         { {pos2.x, pos2.y, 0}, {1, 0} },
     };
-    DirectX11System::Instance().DrawVertices(D3D_PRIMITIVE_TOPOLOGY_LINESTRIP, 2, sizeof(Vertex), vertex);
+    DirectX11System::Instance().DrawVertices(D3D_PRIMITIVE_TOPOLOGY_LINESTRIP, 2, sizeof(vertex), vertex);
 }
 
 void SpriteShader::DrawTriangle(const Math::Vector2& pos1, const Math::Vector2& pos2, const Math::Vector2& pos3, const Math::Color& color, bool fill)
 {
-    // 白テクスチャ
     DirectX11System::Instance().GetCtx()->PSSetShaderResources(0, 1, DirectX11System::Instance().GetWhiteTexture()->GetSrvAddress());
 
-    // 色
-    m_cb0.Get()->Color = color;
-    m_cb0.Write();
-    
-    // 描画
+    m_spriteCB.Get()->color = color;
+    m_spriteCB.Write();
+
     Vertex vertex[] = {
         { {pos1.x, pos1.y, 0}, {0, 0} },
         { {pos2.x, pos2.y, 0}, {1, 0} },
         { {pos3.x, pos3.y, 0}, {0, 0} },
         { {pos1.x, pos1.y, 0}, {1, 0} },
     };
-    DirectX11System::Instance().DrawVertices(
-        fill ? D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP : D3D_PRIMITIVE_TOPOLOGY_LINESTRIP,
-        4, sizeof(Vertex), vertex);
-}
-
-void SpriteShader::DrawCircle(const Math::Vector2& pos, float radius, const Math::Color& color, bool fill)
-{
-    if (radius <= 0.f) return;
-
-    // 白テクスチャ
-    DirectX11System::Instance().GetCtx()->PSSetShaderResources(0, 1, DirectX11System::Instance().GetWhiteTexture()->GetSrvAddress());
-
-    // 色
-    m_cb0.Get()->Color = color;
-    m_cb0.Write();
-
-    // 頂点
-    if (fill) {
-        int faceNum = static_cast<int>(radius) + 1;
-        if (faceNum > 300) {
-            faceNum = 300;
-        }
-        std::vector<Vertex> vertex(faceNum * 3); // 半径により頂点数を調整
-        
-        // 描画
-        for (int i = 0; i < faceNum; ++i) {
-            int idx = i * 3;
-            vertex[idx].Pos.x = pos.x;
-            vertex[idx].Pos.y = pos.y;
-
-            vertex[idx + 1].Pos.x = pos.x + std::cos(DirectX::XMConvertToRadians(i * (360.0f / (faceNum - 1)))) * radius;
-            vertex[idx + 1].Pos.y = pos.y + std::sin(DirectX::XMConvertToRadians(i * (360.0f / (faceNum - 1)))) * radius;
-            vertex[idx + 1].Pos.z = 0;
-
-            vertex[idx + 2].Pos.x = pos.x + std::cos(DirectX::XMConvertToRadians((i + 1) * (360.0f / (faceNum - 1)))) * radius;
-            vertex[idx + 2].Pos.y = pos.y + std::sin(DirectX::XMConvertToRadians((i + 1) * (360.0f / (faceNum - 1)))) * radius;
-            vertex[idx + 2].Pos.z = 0;
-        }
-
-        DirectX11System::Instance().DrawVertices(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST, (int)vertex.size(), sizeof(Vertex), &vertex[0]);
-    }
-    else {
-        int numVertex = static_cast<int>(radius) + 1;
-        if (numVertex > 300) {
-            numVertex = 300;
-        }
-        std::vector<Vertex> vertex(numVertex); // 半径により頂点数を調整
-
-        // 描画
-        for (int i = 0; i < numVertex; ++i) {
-            vertex[i].Pos.x = pos.x + cos(DirectX::XMConvertToRadians(i * (360.0f / (numVertex - 1)))) * radius;
-            vertex[i].Pos.y = pos.y + sin(DirectX::XMConvertToRadians(i * (360.0f / (numVertex - 1)))) * radius;
-            vertex[i].Pos.z = 0;
-        }
-
-        DirectX11System::Instance().DrawVertices(D3D_PRIMITIVE_TOPOLOGY_LINESTRIP, numVertex, sizeof(Vertex), &vertex[0]);
-    }
+    DirectX11System::Instance().DrawVertices(fill ? D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP : D3D_PRIMITIVE_TOPOLOGY_LINESTRIP, 4, sizeof(vertex), vertex);
 }
 
 void SpriteShader::DrawBox(const Math::Vector2& pos, const Math::Vector2& extent, const Math::Color& color, bool fill)
 {
-    // 白テクスチャ
     DirectX11System::Instance().GetCtx()->PSSetShaderResources(0, 1, DirectX11System::Instance().GetWhiteTexture()->GetSrvAddress());
-
-    // 色
-    m_cb0.Get()->Color = color;
-    m_cb0.Write();
+    
+    m_spriteCB.Get()->color = color;
+    m_spriteCB.Write();
 
     Math::Vector3 p1 = { pos.x - extent.x, pos.y - extent.y, 0 };
     Math::Vector3 p2 = { pos.x - extent.x, pos.y + extent.y, 0 };
@@ -322,8 +177,7 @@ void SpriteShader::DrawBox(const Math::Vector2& pos, const Math::Vector2& extent
             { p4, {0, 0}},
             { p3, {0, 0}}
         };
-
-        DirectX11System::Instance().DrawVertices(D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP, 4, sizeof(Vertex), vertex);
+        DirectX11System::Instance().DrawVertices(D3D_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP, 4, sizeof(vertex), vertex);
     }
     else {
         Vertex vertex[] = {
@@ -333,40 +187,53 @@ void SpriteShader::DrawBox(const Math::Vector2& pos, const Math::Vector2& extent
             { p4, {0, 0}},
             { p1, {0, 0}}
         };
-
-        DirectX11System::Instance().DrawVertices(D3D_PRIMITIVE_TOPOLOGY_LINESTRIP, 5, sizeof(Vertex), vertex);
+        DirectX11System::Instance().DrawVertices(D3D_PRIMITIVE_TOPOLOGY_LINESTRIP, 5, sizeof(vertex), vertex);
     }
 }
 
-void SpriteShader::SetScissorRect(const Math::Rectangle& rect)
+void SpriteShader::DrawCircle(const Math::Vector2& pos, float radius, const Math::Color& color, bool fill)
 {
-    ID3D11RasterizerState* rs = directx11_helper::CreateRasterizerState(DirectX11System::Instance().GetDev().Get(), D3D11_FILL_SOLID, D3D11_CULL_BACK, true, true);
-    DirectX11System::Instance().GetCtx()->RSSetState(rs);
-    rs->Release();
-    memory::SafeRelease(&rs);
+    if (radius <= 0.f) return;
+
+    DirectX11System::Instance().GetCtx()->PSSetShaderResources(0, 1, DirectX11System::Instance().GetWhiteTexture()->GetSrvAddress());
     
-    D3D11_RECT rc{};
-    rc.left   = rect.x;
-    rc.top    = rect.y;
-    rc.right  = rect.x + rect.width;
-    rc.bottom = rect.y + rect.height;
-    DirectX11System::Instance().GetCtx()->RSSetScissorRects(1, &rc);
-}
+    m_spriteCB.Get()->color = color;
+    m_spriteCB.Write();
 
-void SpriteShader::Release()
-{
-    memory::SafeRelease(&m_VS);
-    memory::SafeRelease(&m_PS);
-    memory::SafeRelease(&m_VLayout);
-    m_cb0.Release();
-    m_cb1.Release();
-
-    memory::SafeRelease(&m_ds);
-    memory::SafeRelease(&m_rs);
-    memory::SafeRelease(&m_smp0_Point);
-    memory::SafeRelease(&m_smp0_Linear);
-
-    memory::SafeRelease(&m_saveState.DS);
-    memory::SafeRelease(&m_saveState.RS);
-    memory::SafeRelease(&m_saveState.Smp0);
+    if (fill) {
+        int face_count = static_cast<int>(radius) + 1;
+        if (face_count > 300) {
+            face_count = 300;
+        }
+        std::vector<Vertex> vertex(face_count * 3);
+        
+        for (int i = 0; i < face_count; ++i) {
+            int idx = i * 3;
+            vertex[idx].pos.x = pos.x;
+            vertex[idx].pos.y = pos.y;
+            
+            vertex[idx + 1].pos.x = pos.x + std::cos(DirectX::XMConvertToRadians(i * (360.0f / (face_count - 1)))) * radius;
+            vertex[idx + 1].pos.y = pos.y + std::sin(DirectX::XMConvertToRadians(i * (360.0f / (face_count - 1)))) * radius;
+            vertex[idx + 1].pos.z = 0;
+            
+            vertex[idx + 2].pos.x = pos.x + std::cos(DirectX::XMConvertToRadians((i + 1) * (360.0f / (face_count - 1)))) * radius;
+            vertex[idx + 2].pos.y = pos.y + std::sin(DirectX::XMConvertToRadians((i + 1) * (360.0f / (face_count - 1)))) * radius;
+            vertex[idx + 2].pos.z = 0;
+        }
+        DirectX11System::Instance().DrawVertices(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST, (int)vertex.size(), sizeof(vertex), &vertex[0]);
+    }
+    else {
+        int vertex_count = static_cast<int>(radius) + 1;
+        if (vertex_count > 300) {
+            vertex_count = 300;
+        }
+        std::vector<Vertex> vertex(vertex_count); // 半径により頂点数を調整
+        
+        for (int i = 0; i < vertex_count; ++i) {
+            vertex[i].pos.x = pos.x + cos(DirectX::XMConvertToRadians(i * (360.0f / (vertex_count - 1)))) * radius;
+            vertex[i].pos.y = pos.y + sin(DirectX::XMConvertToRadians(i * (360.0f / (vertex_count - 1)))) * radius;
+            vertex[i].pos.z = 0;
+        }
+        DirectX11System::Instance().DrawVertices(D3D_PRIMITIVE_TOPOLOGY_LINESTRIP, vertex_count, sizeof(vertex), &vertex[0]);
+    }
 }
--- Src/System/Shader/SpriteShader/SpriteShader.h
+++ Src/System/Shader/SpriteShader/SpriteShader.h
@@ -1,37 +1,38 @@
 ﻿#pragma once
 
+#include "Math/Convert.h"
+
 class SpriteShader
 {
 public:
 
     struct Vertex {
-        Math::Vector3 Pos;
-        Math::Vector2 UV;
+        Math::Vector3 pos;
+        Math::Vector2 uv;
     };
 
     SpriteShader() {}
     ~SpriteShader() {
         Release();
     }
-    
-    bool Init();
-    
-    // 変換行列セット
-    void SetMatrix(const Math::Matrix& m) {
-        m_cb0.Get()->mTransform = m;
-    }
-    
-    void Begin(bool linear = false, bool disableZBuffer = true);
 
-    void End();
+    bool Init();
+
+    void Release();
+
+    void SetMatrix(const Math::Matrix& matrix) {
+        m_spriteCB.Get()->transform = matrix;
+    }
+
+    void SetToDevice();
 
     void DrawTex(
         const DirectX11Texture& tex,
         const Math::Vector2&    pos,
         const Math::Vector2&    rect,
         const Math::Rectangle*  src_rect = nullptr,
-        const Math::Color&      color   = directx11_helper::white,
-        const Math::Vector2&    pivot   = { 0.5, 0.5f }
+        const Math::Color&      color    = directx11_helper::white,
+        const Math::Vector2&    pivot    = { 0.5, 0.5f }
     );
 
     void DrawTex(
@@ -43,56 +44,115 @@ public:
     ) {
         DrawTex(tex, pos, { static_cast<float>(tex.GetTextureDesc().Width), static_cast<float>(tex.GetTextureDesc().Height) }, src_rect, color, pivot);
     }
-    
+
     void DrawPoint(const Math::Vector2& pos, const Math::Color& color = directx11_helper::white);
 
     void DrawLine(const Math::Vector2& pos1, const Math::Vector2& pos2, const Math::Color& color = directx11_helper::white);
 
     void DrawTriangle(const Math::Vector2& pos1, const Math::Vector2& pos2, const Math::Vector2& pos3, const Math::Color& color = directx11_helper::white, bool fill = true);
-    
-    void DrawCircle(const Math::Vector2& pos, float radius, const Math::Color& color = directx11_helper::white, bool fill = true);
 
     void DrawBox(const Math::Vector2& pos, const Math::Vector2& extent, const Math::Color& color = directx11_helper::white, bool fill = true);
 
-    void SetScissorRect(const Math::Rectangle& rect);
+    void DrawCircle(const Math::Vector2& pos, float radius, const Math::Color& color = directx11_helper::white, bool fill = true);
 
-    void Release();
+private:
+
+    struct SpriteCB {
+        Math::Matrix  transform;                      // 64byte: ワールドビュープロジェクション行列
+        Math::Vector4 color = { 1.f, 1.f, 1.f, 1.f }; // 16byte: 色
+    }; // 80byte
+
+    struct ProjectionCB {
+        Math::Matrix projection; // 64byte: プロジェクション行列
+    }; // 64byte
+
+    DirectX11ConstantBufferSystem<SpriteCB>     m_spriteCB;
+    DirectX11ConstantBufferSystem<ProjectionCB> m_projectionCB;
+
+    ID3D11VertexShader* m_pVS          = nullptr;
+    ID3D11InputLayout*  m_pInputLayout = nullptr;
+    ID3D11PixelShader*  m_pPS          = nullptr;
+    
+};
+
+class SpriteFont
+{
+public:
+
+    SpriteFont() {}
+    ~SpriteFont() {
+        Release();
+    }
+
+    bool Init() {
+        m_upSpriteBatch = std::make_unique<DirectX::SpriteBatch>(DirectX11System::Instance().GetCtx().Get());
+    }
+
+    void Draw(
+        std::string_view     font_name,
+        std::string_view     text,
+        const Math::Vector2& pos,
+        bool                 align_center = false,
+        const Math::Color&   color        = directx11_helper::white,
+        float                scale        = 1.f
+    ) {
+        // フォントデータを取得
+        const auto& font = m_upSpriteFonts.at(font_name.data());
+        // 文字列を作成
+        auto wtext = sjis_to_wide(text.data());
+        // 中心座標を計算
+        Math::Vector2 origin{};
+        if (align_center) {
+            origin = font->MeasureString(wtext.data());
+            convert::ToHalf(origin);
+        }
+
+        // Viewportから補正値を計算
+        D3D11_VIEWPORT vp{};
+        UINT num_viewport = 1;
+        DirectX11System::Instance().GetCtx()->RSGetViewports(&num_viewport, &vp);
+        Math::Vector2 offset_pos {
+            pos.x + convert::ToHalf(vp.Width),
+            -pos.y + convert::ToHalf(vp.Height),
+        };
+
+        font->DrawString(m_upSpriteBatch.get(), wtext.data(), offset_pos, color, 0.f, origin, scale);
+    }
+
+    void Release() {}
+
+    void Begin() {
+        DirectX11System::Instance().GetCtx()->OMGetBlendState(&m_pOldBlendState, m_oldFactor, &m_oldMask);
+        m_upSpriteBatch->Begin(DirectX::SpriteSortMode_Deferred, m_pOldBlendState);
+    }
+
+    void End() {
+        m_upSpriteBatch->End();
+        DirectX11System::Instance().GetCtx()->OMSetBlendState(m_pOldBlendState, m_oldFactor, m_oldMask);
+        memory::SafeRelease(&m_pOldBlendState);
+    }
+
+    void AddFont(std::string_view font_name, std::string_view file_path) {
+        auto wtext = sjis_to_wide(file_path.data());
+        m_upSpriteFonts.emplace(font_name, std::make_unique<DirectX::SpriteFont>(DirectX11System::Instance().GetDev().Get(), wtext.data()));
+    }
+
+    Math::Vector2 MeasureString(std::string_view font_name, std::string_view text) {
+        // フォントデータを取得
+        DirectX::SpriteFont* font = m_upSpriteFonts.at(font_name.data()).get();
+        // 文字列を作成
+        auto wtext = sjis_to_wide(text.data());
+        // 中心座標を計算
+        return font->MeasureString(wtext.data());
+    }
     
 private:
 
-    ID3D11VertexShader* m_VS      = nullptr; // 頂点シェーダー
-    ID3D11InputLayout*  m_VLayout = nullptr; // 頂点レイアウト
-    ID3D11PixelShader*  m_PS      = nullptr; // ピクセルシェーダー
+    std::unique_ptr<DirectX::SpriteBatch>                                 m_upSpriteBatch; // フォント表示用
+    std::unordered_map<std::string, std::unique_ptr<DirectX::SpriteFont>> m_upSpriteFonts; // フォントデータ
 
-    // 定数バッファ
-    struct cbSprite {
-        Math::Matrix mTransform;
-        Math::Vector4 Color = { 1, 1, 1, 1 };
-    };
-    DirectX11ConstantBufferSystem<cbSprite> m_cb0;
-
-    // 定数バッファ
-    struct cbProjection {
-        Math::Matrix mProj;
-    };
-    DirectX11ConstantBufferSystem<cbProjection> m_cb1;
-    
-    // 2D用正射影行列
-    Math::Matrix m_mProj2D;
-
-    // 使用するステート
-    ID3D11DepthStencilState* m_ds          = nullptr;
-    ID3D11RasterizerState*   m_rs          = nullptr;
-    ID3D11SamplerState*      m_smp0_Point  = nullptr;
-    ID3D11SamplerState*      m_smp0_Linear = nullptr;
-
-    // ステート記憶/復元用
-    struct SaveState {
-        ID3D11DepthStencilState* DS         = nullptr;
-        ID3D11RasterizerState*   RS         = nullptr;
-        UINT                     StencilRef = 0;
-        ID3D11SamplerState*      Smp0       = nullptr;
-    };
-    SaveState m_saveState;
+    ID3D11BlendState* m_pOldBlendState = nullptr;
+    FLOAT             m_oldFactor[4]   = {};
+    UINT              m_oldMask        = 0;
     
 };
