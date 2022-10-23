65ffcae83e371d3b36f23448cc57e11768539d4d
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-10-23 22:40:54 +0900

    update: 画面サイズに依存しない場所を計算する関数を追加

--- Src/Application/Game/GameObject/UIObject/UIObject.h
+++ Src/Application/Game/GameObject/UIObject/UIObject.h
@@ -4,4 +4,18 @@
 
 class UIObject : public GameObject
 {
+
+protected:
+    
+    virtual Math::Vector2 GetPositionFromPercentage(float left_based_x_percentage, float top_based_y_percentage) const {
+        // 画面中央を(0, 0)とするためにViewportから補正値を計算
+        auto v = DirectX11System::Instance().GetViewport();
+        return { v.Width * convert::ToUndoPercent(left_based_x_percentage) - convert::ToHalf(v.Width), -v.Height * convert::ToUndoPercent(top_based_y_percentage) + convert::ToHalf(v.Height) };
+    }
+
+    virtual float GetSizePercentageForHDWidthBased() const {
+        auto v = DirectX11System::Instance().GetViewport();
+        return v.Width / Window::HD.first;
+    }
+    
 };
