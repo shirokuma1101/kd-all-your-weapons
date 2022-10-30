0b63fd98bf05b5ae1829ed5bbb1a56f08f784763
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2022-10-30 22:35:14 +0900

    fix: クラス、ファイル名を変更

--- Src/System/Math/CameraProperties.h
+++ Src/System/Math/CameraProperties.h
@@ -2,12 +2,12 @@
 
 #include "ExternalDependencies/Math/Camera.h"
 
-class CameraSystem
+class CameraProperties
 {
 public:
 
-    CameraSystem() = default;
-    CameraSystem(const Math::Matrix& camera_matrix, float fov, float aspect, float near_clipping_distance, float far_clipping_distance)
+    CameraProperties() = default;
+    CameraProperties(const Math::Matrix& camera_matrix, float fov, float aspect, float near_clipping_distance, float far_clipping_distance)
         : m_camera(camera_matrix, fov, aspect, near_clipping_distance, far_clipping_distance)
     {}
 
@@ -21,9 +21,9 @@ public:
 
     void SetToShader() {
         auto camera_cb = DirectX11System::WorkInstance().GetShaderManager()->GetCameraCB().Get();
-        camera_cb->view = m_camera.viewMatrix;
+        camera_cb->view       = m_camera.viewMatrix;
         camera_cb->projection = m_camera.projection.matrix;
-        camera_cb->position = m_camera.cameraMatrix.Translation();
+        camera_cb->position   = m_camera.cameraMatrix.Translation();
 
         DirectX11System::WorkInstance().GetShaderManager()->GetCameraCB().Write();
     }
